fdd17303f564e3b571153fddc33669f8d3acfdc2
Eworitse <ochukoeworitsekolo@gmail.com>
2019-08-08 00:46:47 +0100

    initial commit

---
+++ app/src/main/java/com/osk/travelmanticalc4/InsertTravelDeal.java
@@ -0,0 +1,14 @@
+package com.osk.travelmanticalc4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertTravelDeal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_travel_deal);
+    }
+}
---
+++ app/src/main/java/com/osk/travelmanticalc4/ListTravelDeals.java
@@ -0,0 +1,14 @@
+package com.osk.travelmanticalc4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListTravelDeals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_travel_deals);
+    }
+}
